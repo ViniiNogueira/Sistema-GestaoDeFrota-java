887dfe852011c9052d7411735e348780320330bf
Vinicius Nogueira Martins <viniciusnogueira2507@gmail.com>
2026-02-13 17:48:33 -0300

    Clean up whitespace in Main.java
    Removed unnecessary blank lines in Main.java.

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,6 @@ public class Main {
         veiculos.add(new Caminhao("abc123" , "Scania" , "TH-300", 400, 33.3, "Diesel", "12"));
         veiculos.add(new Moto("cba123","Honda","CB500", 30, 22 ,"gasolina" , 500));
 
-
-
         //chama para manutencao
         veiculos.get(1).enviarManutencao();
 
@@ -19,8 +17,5 @@ public class Main {
             v.viajar(50);
             v.exibirDetalhes();
         }
-
-
-
     }
 }
